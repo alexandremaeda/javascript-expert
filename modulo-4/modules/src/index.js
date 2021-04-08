63caaaef553cadbf0324aa755153cdd0fe717615
Alexandre Maeda <ale.b.maeda@gmail.com>
2021-04-08 17:49:43 -0300

    primeira versão

---
+++ modulo-4/modules/src/index.js
@@ -0,0 +1,27 @@
+'use strict';
+import Draftlog from 'draftlog';
+import chalk from 'chalk';
+import chalkTable from 'chalk-table';
+import readLine from 'readline';
+import database from './../database.json';
+import Person from './person.js';
+
+Draftlog(console).addLineListener(process.stdin);
+
+const DEFAULT_LANG = 'pt-BR';
+const options = {
+  leftPad: 2,
+  columns: [
+    { field: 'id', name: chalk.cyan('ID') },
+    { field: 'vehicles', name: chalk.magenta('Vehicles') },
+    { field: 'kmTraveled', name: chalk.white('KM Traveled') },
+    { field: 'from', name: chalk.blue('From') },
+    { field: 'to', name: chalk.green('To') },
+  ],
+};
+
+const table = chalkTable(
+  options,
+  database.map((item) => new Person(item).formatted(DEFAULT_LANG))
+);
+const print = console.draft(table);
---
+++ modulo-4/modules/src/person.js
@@ -0,0 +1,40 @@
+export default class Person {
+  constructor({ vehicles, kmTraveled, from, to }) {
+    this.id = Date.now();
+    this.vehicles = vehicles;
+    this.kmTraveled = kmTraveled;
+    this.from = from;
+    this.to = to;
+  }
+
+  formatted(language) {
+    const mapDate = (date) => {
+      const [year, month, day] = date.split('-').map(Number);
+
+      // Js inicia o mês 1 sendo 0;
+      return new Date(year, month - 1, day);
+    };
+
+    return {
+      id: Number(this.id),
+      vehicles: new Intl.ListFormat(language, {
+        style: 'long',
+        type: 'conjunction',
+      }).format(this.vehicles),
+      kmTraveled: new Intl.NumberFormat(language, {
+        style: 'unit',
+        unit: 'kilometer',
+      }).format(this.kmTraveled),
+      from: new Intl.DateTimeFormat(language, {
+        month: 'long',
+        day: '2-digit',
+        year: 'numeric',
+      }).format(mapDate(this.from)),
+      to: new Intl.DateTimeFormat(language, {
+        month: 'long',
+        day: '2-digit',
+        year: 'numeric',
+      }).format(mapDate(this.to)),
+    };
+  }
+}
