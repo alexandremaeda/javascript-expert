4ad140b58a715538a157678f9740c54b7fe041e7
Alexandre Maeda <ale.b.maeda@gmail.com>
2022-06-23 08:39:45 -0300

    isValid implementations

--- modulo01/aula01-mocks/src/file.js
+++ modulo01/aula01-mocks/src/file.js
@@ -1,15 +1,22 @@
 const { readFile } = require("fs/promises");
 const { join } = require("path");
+const { error } = require("./constants.js");
 
 const DEFAULT_OPTIONS = {
   maxLines: 3,
   fields: ["id", "name", "profession", "age"],
 };
 
+const REMOVE_BREAK_LINES_REGEX = /(\r\n|\n|\r)/gm;
+
 class File {
   static async csvToJson(filePath) {
     const content = await File.getFileContent(filePath);
-    const isValid = File.isValid(content);
+    const validation = File.isValid(content);
+
+    if (!validation.valid) {
+      throw new Error(validation.error);
+    }
 
     return content;
   }
@@ -17,16 +24,36 @@ class File {
   static async getFileContent(filePath) {
     const fileName = join(__dirname, filePath);
 
-    return (await readFile(fileName)).toString("utf8");
+    const content = (await readFile(fileName)).toString("utf8");
+
+    return content;
   }
 
   static isValid(csvString, options = DEFAULT_OPTIONS) {
-    const lines = csvString.split("\n");
-    console.log(lines);
+    let [header, ...fileWithoutHeader] = csvString.split("\n");
+
+    header = header.replace(REMOVE_BREAK_LINES_REGEX, "");
+    fileWithoutHeader = fileWithoutHeader.map((line) =>
+      line.replace(REMOVE_BREAK_LINES_REGEX, "")
+    );
+
+    const isHeaderValid = header === options.fields.join(",");
+
+    console.log(isHeaderValid);
+
+    if (!isHeaderValid) {
+      return {
+        error: error.FILE_FIELDS_ERROR_MESSAGE,
+        valid: false,
+      };
+    }
+
+    return { valid: true };
   }
 }
 
 (async () => {
   const result = await File.csvToJson("../mocks/threeItems-valid.csv");
+  // const result = await File.csvToJson("../mocks/invalidHeader-invalid.csv");
   console.log(result);
 })();
