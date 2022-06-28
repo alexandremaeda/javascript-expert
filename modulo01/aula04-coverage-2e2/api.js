f8be6a2b5a3829c6c990f5fcf23e5609c0b504e5
Alexandre Maeda <ale.b.maeda@gmail.com>
2022-06-28 08:32:54 -0300

    aula04-coverage-2e2

---
+++ modulo01/aula04-coverage-2e2/api.js
@@ -0,0 +1,30 @@
+const http = require("http");
+
+const routes = {
+  "/contact:get": (request, response) => {
+    response.write("contact us page");
+    return response.end();
+  },
+  default: (request, response) => {
+    response.write("hello world");
+    return response.end();
+  },
+};
+
+const handler = function (request, response) {
+  const { url, method } = request;
+  const routeKey = `${url}:${method.toLowerCase()}`;
+  const chosen = routes[routeKey] || routes.default;
+  response.writeHead(200, {
+    "Content-Type": "text/html",
+  });
+
+  return chosen(request, response);
+};
+
+// curl -i localhost:3000
+const app = http
+  .createServer(handler)
+  .listen(3000, () => console.log("Server is online at:", 3000));
+
+module.exports = app;
---
+++ modulo01/aula04-coverage-2e2/api.test.js
@@ -0,0 +1,13 @@
+const { describe, it } = require("mocha");
+const request = require("supertest");
+const app = require("./api");
+
+describe("Api Suite test", () => {
+  describe("/contact", () => {
+    it("shold request the contact page and return HTTP Status 200", async () => {
+      const response = await request(app).get("/contact").expect(200);
+
+      console.log(response);
+    });
+  });
+});
