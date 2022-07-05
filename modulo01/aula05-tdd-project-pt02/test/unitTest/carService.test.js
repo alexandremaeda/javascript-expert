b4abdd6d1814d2a506166fc51c028f35b84bb594
Alexandre Maeda <ale.b.maeda@gmail.com>
2022-07-05 08:20:28 -0300

    continuing

--- modulo01/aula05-tdd-project-pt02/test/unitTest/carService.test.js
+++ modulo01/aula05-tdd-project-pt02/test/unitTest/carService.test.js
@@ -2,8 +2,14 @@ const { describe, it, before } = require("mocha");
 const CarService = require("../../src/service/CarService");
 
 const { join } = require("path");
-
+const { expect } = require("chai");
 const carDatabase = join(__dirname, "./../../database", "cars.json");
+
+const mocks = {
+  validCarCategory: require("../mocks/valid-carCategory.json"),
+  validCar: require("../mocks/valid-car.json"),
+  validCustomer: require("../mocks/valid-customer.json"),
+};
 
 describe("CarService Suite Test", () => {
   let carService = {};
@@ -12,10 +18,21 @@ describe("CarService Suite Test", () => {
     carService = new CarService({ cars: carDatabase });
   });
 
-  it("given a carCategory it should return an available car", async () => {
-    const result = await carService.test(
-      "75fb798a-3829-4fbf-82a1-5a27ee33ab3a"
-    );
-    console.log(result);
+  it("should retrieve a random postition from an array", () => {
+    const data = [0, 1, 2, 3, 4];
+    const result = carService.getRandomPositionFromArray(data);
+
+    expect(result).to.be.lte(data.length).and.be.gte(0);
   });
+
+  // it("given a carCategory it should return an available car", async () => {
+  //   const car = mocks.validCar;
+  //   const carCategory = Object.create(mocks.validCarCategory);
+  //   carCategory.ids = [car.id];
+
+  //   const result = await carService.getAvailableCar(carCategory);
+  //   const expected = car;
+
+  //   expect(result).to.be.deep.equal(expected);
+  // });
 });
